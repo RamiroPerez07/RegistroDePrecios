0f4b4bb4b5771004d553edd05cc3003982f549bd
RamiroPerez07 <ramiperez1997@hotmail.com>
2025-03-07 19:02:02 -0300

    mod login y register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,45 @@
 import { Request, Response } from 'express';
+import User, { IUser } from './auth.model';
+import jwt from 'jsonwebtoken';
 
 export class AuthController {
-  login(req: Request, res: Response) {
-    // Lógica de login
-    res.json({ message: 'Login exitoso' });
+
+  secret = "este_es_un_secreto_que_debe_ser_reemplazado_por_otro"
+
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    try {
+      const user: IUser | null = await User.findOne({ email });
+      if (!user) {
+        res.status(404).json({ message: 'Usuario no encontrado' });
+        return;
+      }
+
+      const isMatch = await user.comparePassword(password);
+      if (!isMatch) {
+        res.status(400).json({ message: 'Contraseña incorrecta' });
+        return;
+      }
+
+      // Generar un token JWT
+      const token = jwt.sign({ id: user._id }, this.secret, { expiresIn: '1h' });
+      
+      res.status(200).json({user, token });
+    } catch (error) {
+      res.status(500).json({ message: 'Error del servidor', error });
+    }
   }
 
-  register(req: Request, res: Response) {
-    // Lógica de registro
-    res.json({ message: 'Registro exitoso' });
+  async register(req: Request, res: Response) {
+    const { username, email, password } = req.body;
+
+  try {
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).json({ message: 'Usuario registrado exitosamente' });
+  } catch (error) {
+    res.status(400).json({ message: 'Error al registrar el usuario', error });
+  }
   }
 }
---
+++ src/auth/auth.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  comparePassword(password: string): Promise<boolean>;
+}
+
+const UserSchema: Schema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Hashear la contraseña antes de guardar el usuario
+UserSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')){
+    next();
+    return;
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Método para comparar contraseñas
+UserSchema.methods.comparePassword = function (password: string): Promise<boolean> {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model<IUser>('User', UserSchema);
+
+export default User;
